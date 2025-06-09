3900d3220e4263ccfaade2e02a564082f3fdabde
Rafael Alves <rafaalves1921@gmail.com>
2025-06-09 12:57:36 +0100

    Login

--- frontend/tasty-check/src/components/MainPage/TopNavbar.vue
+++ frontend/tasty-check/src/components/MainPage/TopNavbar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="absolute top-0 w-full z-10 bg-transparent">
     <div class="container-fluid flex justify-end p-3">
-      <a href="/login" class="btn !bg-[#095243] w-[157px] hover:bg-[#073b31] text-white font-bold text-xs py-[8px] px-[24px] rounded-lg transition-colors duration-300">
+      <router-link to="/login" class="btn !bg-[#095243] w-[157px] hover:bg-[#073b31] text-white font-bold text-xs py-[8px] px-[24px] rounded-lg transition-colors duration-300">
         Iniciar Sessão
-      </a>
+      </router-link>
     </div>
   </nav>
 </template>
